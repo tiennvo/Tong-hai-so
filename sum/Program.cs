38bec7d70a33afe8afb8d609d8e70ff18d9968c9
tiennvo <ccoderr1@gmail.com>
2024-03-21 23:41:33 +0700

    sum

---
+++ sum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Nhap a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tong 2 so la: " + (a + b));
+        }
+    }
+}
